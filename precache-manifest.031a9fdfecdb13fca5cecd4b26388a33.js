c5f35e3f89df3b33c2d32cb88eda32e4805a1be8
Shaun <shaunbw4@gmail.com>
2021-03-10 12:58:08 +0200

    Updates

---
+++ precache-manifest.031a9fdfecdb13fca5cecd4b26388a33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0ed0be4bb6b57b1e73c1320d2b892c3",
+    "url": "/react-quote-generator/index.html"
+  },
+  {
+    "revision": "4e6c2af96fa5d91bd5ea",
+    "url": "/react-quote-generator/static/css/main.afc86a99.chunk.css"
+  },
+  {
+    "revision": "c1ffe71b46e5f6cf9c80",
+    "url": "/react-quote-generator/static/js/2.18290fbb.chunk.js"
+  },
+  {
+    "revision": "4e6c2af96fa5d91bd5ea",
+    "url": "/react-quote-generator/static/js/main.570e7893.chunk.js"
+  },
+  {
+    "revision": "b6fd0dcbc944fc9c3f7d",
+    "url": "/react-quote-generator/static/js/runtime-main.cd62d776.js"
+  },
+  {
+    "revision": "5b7eb27173bcb62344bbb1959cc74251",
+    "url": "/react-quote-generator/static/media/twitter_icon.5b7eb271.png"
+  }
+]);
